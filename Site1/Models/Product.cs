87aa9cb210865340c61d27c2fac75b35807f03f3
xanderyanov <xanderyanov@gmail.com>
2022-03-14 10:12:49 +0400

    First commit

---
+++ Site1/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace Site1.Models
+{
+    public class Product
+    {
+        public long ProductID { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+
+    }
+}
